d96491edee676d3c6341339e9e419eb480a19707
PusarlaSriRani <163016669+PusarlaSriRani@users.noreply.github.com>
2026-06-09 13:37:41 +0530

    insert_employees

---
+++ 02_insert_one.js
@@ -0,0 +1,20 @@
+db.employee.insertOne({
+    name: "Sri",
+    age: 22,
+    department: "AI & ML",
+    salary: 35000
+})
+
+db.employee.insertOne({
+    name: "Rani",
+    age: 21,
+    department: "CSE",
+    salary: 30000
+})
+
+db.employee.insertOne({
+    name: "Vijay",
+    age: 23,
+    department: "ECE",
+    salary: 40000
+})
