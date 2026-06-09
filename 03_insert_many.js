c4c85bd663016f95af482c555f43bba9e4e2e5e4
PusarlaSriRani <163016669+PusarlaSriRani@users.noreply.github.com>
2026-06-09 13:41:23 +0530

    insert_many.js

---
+++ 03_insert_many.js
@@ -0,0 +1,20 @@
+db.employee.insertMany([
+{
+    name: "Kiran",
+    age: 24,
+    department: "IT",
+    salary: 42000
+},
+{
+    name: "Priya",
+    age: 22,
+    department: "AI & ML",
+    salary: 39000
+},
+{
+    name: "Anil",
+    age: 25,
+    department: "CSE",
+    salary: 45000
+}
+])
